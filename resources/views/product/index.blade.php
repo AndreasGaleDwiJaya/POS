37df1a95fe77e1b17c2250a1d5d65c5878e80af1
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-09-05 09:28:04 +0700

    Pertemuan 2 - Soal Praktikum

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Daftar Produk di Toko Kami</h1>
+</body>
+</html>
---
+++ resources/views/sales.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Penjualan</title>
+</head>
+<body>
+    <h1>Berikut adalah Penjualan Barang di Web POS</h1>
+</body>
+</html>
